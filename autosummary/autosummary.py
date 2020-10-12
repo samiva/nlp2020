aea03ef9cac9b9aa8720def05c63034ee602b357
Toni Bomström <toni.bomstrom@gmail.com>
2020-10-12 19:08:56 +0300

    Parse titles, headers and abstract from HTML
    Use BeautifulSoup (bs4) for parsing the titles, headers and the abstract
    (if it exists) from the raw html.
    
    Addresses: Task 1

--- autosummary/autosummary.py
+++ autosummary/autosummary.py
@@ -6,11 +6,23 @@ the CLI and the actual automatic summarization functionality.
 
 import argparse
 import logging
+import re
 import urllib.request
 
+from typing import Sequence
+
+from bs4 import BeautifulSoup
 
 _logger = logging.getLogger(__name__)
 _LOGGING_FORMAT = "%(asctime)s %(module)s [%(levelname)s]: %(message)s"
+
+
+def _abstract_from_html(html: str) -> str:
+    soup = BeautifulSoup(html, "html.parser")
+    for header in soup.find_all(re.compile('^h[1-6]$')):
+        if header.get_text().upper() == "ABSTRACT":
+            # Get the text of the following item after the abstract header
+            return header.next_sibling.get_text()
 
 
 def _argument_parser() -> argparse.ArgumentParser:
@@ -50,8 +62,25 @@ def main():
     source_type = _parsed_args.source_type
     source_path = _parsed_args.source_path
     _logger.debug("Using source '{}' ({})".format(source_path, source_type.upper()))
+
     raw_html = _get_raw_source(source_type, source_path)
-    _logger.debug(raw_html)
+    # Get the titles & headers
+    titles = _titles_from_html(raw_html)
+    _logger.debug("TITLES: {}".format(titles))
+    # Get the abstract (if it exists)
+    abstract = _abstract_from_html(raw_html)
+    _logger.debug("ABSTRACT: {}".format(abstract))
+
+
+def _titles_from_html(html: str) -> Sequence[str]:
+    """Returns the title(s) and headers as a list."""
+    soup = BeautifulSoup(html, "html.parser")
+    # Get the title(s)
+    titles = [a.get_text() for a in soup.find_all("title")]
+    # Get all headers in the order of appearance
+    # https://stackoverflow.com/questions/45062534/how-to-grab-all-headers-from-a-website-using-beautifulsoup
+    titles.extend(a.get_text().strip("¶") for a in soup.find_all(re.compile('^h[1-6]$')))
+    return titles
 
 
 if __name__ == "__main__":
